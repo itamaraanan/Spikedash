06062ef758a17b9b604374eab61a4a1c04f49d9e
itamar <pitaita1109@gmail.com>
2024-12-03 22:00:03 +0200

    added screen touch

--- app/src/main/java/com/example/spikedash_singleplayer/GameActivity.java
+++ app/src/main/java/com/example/spikedash_singleplayer/GameActivity.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.FrameLayout;
 
 public class GameActivity extends AppCompatActivity {
 
     FrameLayout frm;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,18 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         frm = findViewById(R.id.frm);
+
+        View rootLayout = findViewById(R.id.root_layout);
+        rootLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    Log.d("ScreenTouch", "Screen was touched!");
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
