5cf4d9a515cc504f2e10b7d4a1cd074e650e99e5
Itamar Ra'anan <raananitamar@gmail.com>
2025-02-18 15:45:55 +0200

    added stats menu2

--- app/src/main/java/com/example/spikedash_singleplayer/StatsActivity.java
+++ app/src/main/java/com/example/spikedash_singleplayer/StatsActivity.java
@@ -15,7 +15,6 @@ public class StatsActivity extends AppCompatActivity implements View.OnClickList
 
     ImageButton btnReturn;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
