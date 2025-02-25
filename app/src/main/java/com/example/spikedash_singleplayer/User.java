dd6dc4b86caaaa2e7ad21f169ac5388e7551df46
Itamar Ra'anan <raananitamar@gmail.com>
2025-02-25 21:55:17 +0200

    added forgot option

--- app/src/main/java/com/example/spikedash_singleplayer/User.java
+++ app/src/main/java/com/example/spikedash_singleplayer/User.java
@@ -3,13 +3,26 @@ package com.example.spikedash_singleplayer;
 public class User {
     public String username;
     public String email;
+    private int balance;
 
     public User() {
-        // Default constructor required for Firebase
     }
 
     public User(String username, String email) {
         this.username = username;
         this.email = email;
+        this.balance = 0;
     }
+
+    public void add(int amount) {
+        this.balance += amount;
+    }
+    public void subtract(int amount) {
+        this.balance -= amount;
+    }
+    public int getBalance() {
+        return this.balance;
+    }
+
+
 }
