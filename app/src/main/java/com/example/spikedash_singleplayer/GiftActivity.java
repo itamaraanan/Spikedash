a33ae15f50c82e310ae59454537cec0c08b060b2
Itamar Ra'anan <raananitamar@gmail.com>
2025-04-24 22:48:01 +0300

    added countDown to the wheel spin

--- app/src/main/java/com/example/spikedash_singleplayer/GiftActivity.java
+++ app/src/main/java/com/example/spikedash_singleplayer/GiftActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bluehomestudio.luckywheel.LuckyWheel;
@@ -23,7 +26,7 @@ import java.util.Random;
 
 public class GiftActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    TextView tvTimer;
     Button spinButton;
     ImageButton btnReturnMenu;
     LuckyWheel luckyWheel;
@@ -32,6 +35,8 @@ public class GiftActivity extends AppCompatActivity implements View.OnClickListe
     String pointsAmount;
     List<WheelItem> wheelItemList = new ArrayList<>();
     String points;
+    CountDownTimer countdownTimer;
+    final long TWENTY_FOUR_HOURS_MS = 24 * 60 * 60 * 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +49,28 @@ public class GiftActivity extends AppCompatActivity implements View.OnClickListe
         intent = getIntent();
         btnReturnMenu.setOnClickListener(this);
         spinButton.setOnClickListener(this);
+        tvTimer = findViewById(R.id.tvTimer);
+
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
+        userRef.child("lastSpinTime").get().addOnSuccessListener(snapshot -> {
+            if (snapshot.exists()) {
+                long lastSpinTime = snapshot.getValue(Long.class);
+                long timeSinceLastSpin = System.currentTimeMillis() - lastSpinTime;
+
+                if (timeSinceLastSpin < TWENTY_FOUR_HOURS_MS) {
+                    startCooldown(TWENTY_FOUR_HOURS_MS - timeSinceLastSpin);
+                } else {
+                    spinButton.setVisibility(View.VISIBLE);
+                    tvTimer.setVisibility(View.INVISIBLE);
+                }
+            } else {
+                spinButton.setEnabled(true);
+                tvTimer.setVisibility(View.INVISIBLE);
+            }
+        }).addOnFailureListener(e -> {
+            spinButton.setEnabled(true);
+            tvTimer.setVisibility(View.INVISIBLE);
+        });
 
     WheelItem gift1 = new WheelItem(ResourcesCompat.getColor(getResources(), R.color.blue, null), BitmapFactory.decodeResource(getResources(), R.drawable.ic_random_skin));
 WheelItem gift2 = new WheelItem(ResourcesCompat.getColor(getResources(), R.color.orange, null), BitmapFactory.decodeResource(getResources(), R.drawable.ic_candyy), "10");
@@ -82,6 +109,37 @@ WheelItem gift6 = new WheelItem(ResourcesCompat.getColor(getResources(), R.color
 
     }
 
+    private void startCooldown(long millisUntilFinished) {
+        tvTimer.setVisibility(View.VISIBLE);
+        spinButton.setVisibility(View.INVISIBLE);
+
+        if (countdownTimer != null) {
+            countdownTimer.cancel();
+        }
+
+        countdownTimer = new CountDownTimer(millisUntilFinished, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                long seconds = millisUntilFinished / 1000;
+                long hours = seconds / 3600;
+                long minutes = (seconds % 3600) / 60;
+                long secs = seconds % 60;
+
+                String time = String.format("%02d:%02d:%02d", hours, minutes, secs);
+                tvTimer.setText("Come back in: " + time);
+            }
+
+            @Override
+            public void onFinish() {
+                tvTimer.setVisibility(View.INVISIBLE);
+                spinButton.setVisibility(View.VISIBLE);
+            }
+        };
+
+        countdownTimer.start();
+    }
+
+
     @Override
     public void onClick(View v) {
         if(v == spinButton) {
@@ -90,6 +148,12 @@ WheelItem gift6 = new WheelItem(ResourcesCompat.getColor(getResources(), R.color
             Random random = new Random();
             points = String.valueOf(random.nextInt(10));
             luckyWheel.rotateWheelTo(Integer.parseInt(points));
+
+            spinButton.setVisibility(View.INVISIBLE);
+            DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
+            userRef.child("lastSpinTime").setValue(System.currentTimeMillis());
+            startCooldown(TWENTY_FOUR_HOURS_MS);
+
         }
         else if (v == btnReturnMenu){
             Intent intent = new Intent(GiftActivity.this, MainActivity.class);
